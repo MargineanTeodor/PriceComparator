8d736a70ed7568c6d6f1aee0dd25167ae10801a9
MargineanTeodor <119050817+MargineanTeodor@users.noreply.github.com>
2025-05-18 12:07:22 +0300

    initial commit
    Spring boot initializr

---
+++ src/test/java/PriceComparator/PriceComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package PriceComparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
